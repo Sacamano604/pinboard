a6dff5537e8378b558ecef63474d4c4429a6d2d2
Benjamin Toussi <ben@Benjamins-Air.hitronhub.home>
2017-03-22 21:55:47 -0700

    More firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  message: string = '';
+  
+  constructor(public af: AngularFire) {
+    this.items = af.database.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  
+
+
+
 }
+
