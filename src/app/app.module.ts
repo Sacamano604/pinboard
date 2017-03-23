578ea1a4e217c702d97206471a734f9a8579ca49
Benjamin Toussi <ben@Benjamins-Air.hitronhub.home>
2017-03-22 21:47:24 -0700

    Adding firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7W5NYhphz2y4tEdMOgzgq_8xMYf1M28Q",
+  authDomain: "pin-board-20cff.firebaseapp.com",
+  databaseURL: "https://pin-board-20cff.firebaseio.com",
+  storageBucket: "pin-board-20cff.appspot.com",
+  messagingSenderId: "789538035078"
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +21,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
